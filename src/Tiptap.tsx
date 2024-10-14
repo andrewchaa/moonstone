adf640051e4177f29dada1421a15629780ec912a
Youngho Chaa <youngho.chaa@starlingbank.com>
2024-10-14 17:37:29 +0100

    Use EditorProvider. Somehow useEditor hook didn't work

--- src/Tiptap.tsx
+++ src/Tiptap.tsx
@@ -1,26 +1,16 @@
-import { useEditor, EditorContent, FloatingMenu, BubbleMenu } from '@tiptap/react';
-import StarterKit from '@tiptap/starter-kit';
 import React from 'react';
+import { EditorProvider, FloatingMenu, BubbleMenu } from '@tiptap/react';
+import StarterKit from '@tiptap/starter-kit';
 
 const extensions = [StarterKit]
 const content = '<p>Hello World! 🌍️</p>'
 
 const Tiptap = () => {
-  const editor = useEditor({
-    extensions,
-    content,
-  })
-
   return (
-    <>
-      <EditorContent editor={editor} />
-      <FloatingMenu editor={null}>
-        This is the floating menu
-      </FloatingMenu>
-      <BubbleMenu editor={null}>
-        This is the bubble menu
-      </BubbleMenu>
-    </>
+    <EditorProvider extensions={extensions} content={content}>
+    <FloatingMenu editor={null}>This is the floating menu</FloatingMenu>
+    <BubbleMenu editor={null}>This is the bubble menu</BubbleMenu>
+  </EditorProvider>
   )
 }
 
