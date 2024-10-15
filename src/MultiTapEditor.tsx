9e2e01057739d380c8e68cadd92106a0e75dc156
Youngho Chaa <youngho.chaa@starlingbank.com>
2024-10-15 23:39:08 +0100

    Support multi tabs with HTML

--- src/MultiTapEditor.tsx
+++ src/MultiTapEditor.tsx
@@ -12,6 +12,38 @@ interface Tab {
   content: string
 }
 
+const initialContent = `
+<h2>
+  Hi there,
+</h2>
+<p>
+  this is a <em>basic</em> example of <strong>Tiptap</strong>. Sure, there are all kind of basic text styles you’d probably expect from a text editor. But wait until you see the lists:
+</p>
+<ul>
+  <li>
+    That’s a bullet list with one …
+  </li>
+  <li>
+    … or two list items.
+  </li>
+</ul>
+<p>
+  Isn’t that great? And all of that is editable. But wait, there’s more. Let’s try a code block:
+</p>
+<pre><code class="language-css">body {
+  display: none;
+}</code></pre>
+<p>
+  I know, I know, this is impressive. It’s only the tip of the iceberg though. Give it a try and click a little bit around. Don’t forget to check the other examples too.
+</p>
+<blockquote>
+  Wow, that’s amazing. Good work, boy! 👏
+  <br />
+  — Mom
+</blockquote>
+`
+
+
 export default function MultiTabEditor() {
   const [tabs, setTabs] = useState<Tab[]>([
     { id: '1', title: 'Tab 1', content: '' }
@@ -73,8 +105,8 @@ export default function MultiTabEditor() {
         {tabs.map(tab => (
           <TabsContent key={tab.id} value={tab.id} className="mt-0">
             <Tiptap
-              htmlContent={tab.content}
-              handleUpdate={(content) => updateTabContent(tab.id, content)}
+              content={tab.content}
+              handleUpdate={(contentUpdate) => updateTabContent(tab.id, contentUpdate)}
             />
             {/* <Textarea
               value={tab.content}
--- src/Tiptap.tsx
+++ src/Tiptap.tsx
@@ -5,8 +5,10 @@ import ListItem from '@tiptap/extension-list-item'
 import TextStyle from '@tiptap/extension-text-style'
 import Highlight from '@tiptap/extension-highlight'
 import Typography from '@tiptap/extension-typography'
+import { Markdown } from 'tiptap-markdown'
 import { EditorProvider } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
+
 
 const extensions = [
   Color.configure({ types: [TextStyle.name, ListItem.name] }),
@@ -22,55 +24,23 @@ const extensions = [
     },
   }),
   Highlight,
+  Markdown,
   Typography,
 ]
 
-const content = `
-<h2>
-  Hi there,
-</h2>
-<p>
-  this is a <em>basic</em> example of <strong>Tiptap</strong>. Sure, there are all kind of basic text styles you’d probably expect from a text editor. But wait until you see the lists:
-</p>
-<ul>
-  <li>
-    That’s a bullet list with one …
-  </li>
-  <li>
-    … or two list items.
-  </li>
-</ul>
-<p>
-  Isn’t that great? And all of that is editable. But wait, there’s more. Let’s try a code block:
-</p>
-<pre><code class="language-css">body {
-  display: none;
-}</code></pre>
-<p>
-  I know, I know, this is impressive. It’s only the tip of the iceberg though. Give it a try and click a little bit around. Don’t forget to check the other examples too.
-</p>
-<blockquote>
-  Wow, that’s amazing. Good work, boy! 👏
-  <br />
-  — Mom
-</blockquote>
-`
+type Props = {
+  content: string
+  handleUpdate: (content: string) => void
+}
 
-const Tiptap = ({
-  htmlContent,
-  handleUpdate,
-}) => {
+const Tiptap = ({ content, handleUpdate }: Props) => {
   return (
     <div className="tiptap">
       <EditorProvider
-        // onUpdate={handleUpdate}
         onUpdate={
-          ({ editor }) => {
-            console.log(editor.getHTML())
-            handleUpdate(editor.getHTML())
-          }}
+          ({ editor }) => { handleUpdate(editor.getHTML()) }}
         extensions={extensions}
-        content={htmlContent}
+        content={content}
       ></EditorProvider>
     </div>
   )
