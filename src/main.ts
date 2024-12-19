91bd998d5dffae6f59994e9b9d56eb8fc275102a
Youngho Chaa <youngho.chaa@starlingbank.com>
2024-12-19 11:17:48 +0000

    Open url only in the browser

--- src/main.ts
+++ src/main.ts
@@ -42,7 +42,7 @@ const createWindow = async () => {
 
   mainWindow.webContents.setWindowOpenHandler(({ url }) => {
     shell.openExternal(url);
-    return { action: 'allow' }
+    return { action: 'deny' }
   })
 
   return mainWindow;
